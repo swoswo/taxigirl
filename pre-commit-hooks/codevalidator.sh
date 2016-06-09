cb1897d591472760db182df3355f1dbbc2f1d5a8
Tom Hensel <github@jitter.eu>
2016-06-10 01:31:18 +0200

    sync regexp

--- pre-commit-hooks/codevalidator.sh
+++ pre-commit-hooks/codevalidator.sh
@@ -7,7 +7,7 @@ html|less|css|php|js)|\
 Gemfile|Brewfile.*|Vagrantfile$"
 
 for f in $(git diff --cached --name-only | grep -E "$FILES_PATTERN"); do
-    if ! codevalidator "$f"; then
+    if ! codevalidator -f -v "$f"; then
         exit 1
     fi
 done
