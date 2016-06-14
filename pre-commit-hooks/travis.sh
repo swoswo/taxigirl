8c7686672c2b5496279dcc52b8105c1dcc43c03f
Tom Hensel <github@jitter.eu>
2016-06-14 22:26:22 +0200

    re-add Gemfile lint .travis.yml

---
+++ pre-commit-hooks/travis.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+FILES_PATTERN='.travis.yml$'
+
+for f in $(git diff --cached --name-only | grep -E "$FILES_PATTERN"); do
+    if ! bundle exec travis lint -qx "$f"; then
+        exit 1
+    fi
+done
