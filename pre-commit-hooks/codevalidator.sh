d78d02786bb38e1d46356bfe693ec7f80bbe7383
Tom Hensel <github@jitter.eu>
2016-06-15 02:48:08 +0200

    dont let codevalidator interfere with the other python linters tweak order

--- pre-commit-hooks/codevalidator.sh
+++ pre-commit-hooks/codevalidator.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 #
 FILES_PATTERN=\
-".*\.(yaml|yml|json|py|rb|erb|sql|sh|xml|\
+".*\.(yaml|yml|json|rb|erb|sql|sh|xml|\
 java|jsp|vm|properties|\
 html|less|css|php|js)|\
 Gemfile|Brewfile.*|Vagrantfile$"
+
+# PY_PATTERN=".*\.(py)"
 
 for f in $(git diff --cached --name-only | grep -E "$FILES_PATTERN"); do
     if ! codevalidator -f -v "$f"; then
         exit 1
     fi
 done
+
+# for f in $(git diff --cached --name-only | grep -E "$PY_PATTERN"); do
+#     if ! codevalidator -a pythontidy -v "$f"; then
+#         exit 1
+#     fi
+# done
