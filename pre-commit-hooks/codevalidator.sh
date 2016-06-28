bcb1a5269a6c74c57999d9869cb1e0bfa1f6552a
Tom Hensel <github@jitter.eu>
2016-06-28 22:49:36 +0200

    enable provisioning of ec2 instances quality cleanup
    [skip ci]

--- pre-commit-hooks/codevalidator.sh
+++ pre-commit-hooks/codevalidator.sh
@@ -8,7 +8,7 @@ Gemfile|Brewfile.*|Vagrantfile$"
 
 # PY_PATTERN=".*\.(py)"
 
-for f in $(git diff --cached --name-only | grep -E "$FILES_PATTERN"); do
+for f in $(git diff --name-only | grep -E "$FILES_PATTERN"); do
     if ! codevalidator -f -v "$f"; then
         exit 1
     fi
